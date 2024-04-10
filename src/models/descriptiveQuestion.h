f60fe29e725db78367409877205183a1523c1c38
Erfan Sadeghinezhad <erfansadeghinezhad@gmail.com>
2024-04-10 18:31:34 +0330

    feat: add descriptiveQuestion model

---
+++ src/models/descriptiveQuestion.h
@@ -0,0 +1,15 @@
+//
+// Created by erfan on 4/10/2024.
+//
+
+#ifndef GOOGLE_FORM_DESCRIPTIVEQUESTION_H
+#define GOOGLE_FORM_DESCRIPTIVEQUESTION_H
+
+#include "question.h"
+
+class DescriptiveQuestion : public Question {
+public:
+    std::string answer;
+};
+
+#endif//GOOGLE_FORM_DESCRIPTIVEQUESTION_H
