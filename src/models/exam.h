613260997af7a40f532fcbde0ef7adc26729b0be
alirhw <ali.rouhani.2005@gmail.com>
2024-04-11 15:22:35 +0430

    update exam model

--- src/models/exam.h
+++ src/models/exam.h
@@ -1,4 +1,5 @@
-//
+#endif//GOOGLE_FORM_EXAM_H
+///
 // Created by erfan on 4/10/2024.
 //
 
@@ -11,13 +12,16 @@
 
 class Exam {
 public:
+	 Exam(int _examId, std::string _examName, std::string _examDate, std::string _examTime, double _totalScore);
     int examId;
     std::string examName;
     std::string examDate;
     std::string examTime;
     double totalScore;
-    Question *questions[100];                      // Assuming a maximum of 100 questions
+    Question *questions[100]{};                      // Assuming a maximum of 100 questions
     std::pair<std::string, int> studentScores[100];// Pair of student username and score
+    void saveToFile(const std::string &filename) const;
 };
 
 #endif//GOOGLE_FORM_EXAM_H
+
