958ea8be9bc7845c99fc203f138257abc1dc91bb
alirhw <ali.rouhani.2005@gmail.com>
2024-04-17 01:02:50 +0430

    add create question to display

--- src/views/display.hpp
+++ src/views/display.hpp
@@ -64,4 +64,17 @@ public:
         std::cin >> name;
         std::cout << "Creating Student Group:" <<  name << std::endl;
     }
+    static void CreateQuestion(int &time, std::string &prompt, std::string &description,double &score,type &type) {
+	
+	std::cout << "Please enter type   Descriptive=0   MultipleChoice=1" << std::endl;
+	std::cin>>type;///////????????
+    std::cout << "Please enter prompt" << std::endl;
+    getline(std::cin, prompt);
+    std::cout << "Please enter description" << std::endl;
+    getline(std::cin, description);
+    std::cout << "Please enter  time" << std::endl;
+    std::cin >>time;
+    std::cout << "Please enter  score" << std::endl;
+    std::cin >>score;
+}
 };
