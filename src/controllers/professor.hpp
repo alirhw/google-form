8fb34dda0b3055a6af94b0cf60ea85280e9b875b
alirhw <ali.rouhani.2005@gmail.com>
2024-04-17 01:44:43 +0430

    Add createExam function to controllers

--- src/controllers/professor.hpp
+++ src/controllers/professor.hpp
@@ -3,6 +3,7 @@
 //
 
 #include "../models/studentGroup.h"
+#include "../models/exam.h"
 
 class Manager {
 public:
@@ -10,4 +11,8 @@ public:
         StudentGroup studentGroup(name);
         studentGroup.saveToFile("data/studentGroups.csv");
     }
+    static void createExam(int examId, std::string examName, std::string examDate, std::string examTime, double totalScore){
+    	Exam exam( examId,  examName, examDate,  examTime,  totalScore);
+    	exam.saveToFile("data/Exam.csv");
+	}
 };
