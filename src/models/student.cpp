188c5d907b50dd15dbef237c5ce848c4bd0c01c6
Erfan Sadeghinezhad <erfansadeghinezhad@gmail.com>
2024-04-17 20:19:01 +0330

    feat: create findByUsername methods for searching

--- src/models/student.cpp
+++ src/models/student.cpp
@@ -3,6 +3,36 @@
 //
 
 #include "student.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 Student::Student(std::string fullname, std::string username, std::string password)
     : User(std::move(fullname), std::move(username), std::move(password)) {}
+
+Student Student::findByUsername(const std::string &filename, std::string username) {
+    std::ifstream inFile(filename, std::ios::app);
+    // Check if file opened successfully
+    if (!inFile.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    std::string line;
+    while (std::getline(inFile, line)) {
+        std::vector<std::string> fields;
+        std::string token;
+        std::stringstream ss(line);
+        while (getline(ss, token, ',')) {
+            fields.push_back(token);
+        }
+        for (int i = 0; i < fields.size(); ++i) {
+            if (fields.at(0) == username && fields.at(2) == "student") {
+                Student found(fields.at(3), fields.at(0), fields.at(1));
+                return found;
+            }
+        }
+    }
+    inFile.close();
+}
