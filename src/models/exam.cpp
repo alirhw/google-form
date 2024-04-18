f3125091016ab2e23b8731ab6dd517fd5ac8ceb7
alirhw <ali.rouhani.2005@gmail.com>
2024-04-18 19:37:59 +0430

    update exam model

--- src/models/exam.cpp
+++ src/models/exam.cpp
@@ -20,46 +20,59 @@ Exam::Exam(int examId, std::string examName, std::string examDate, std::string e
 }
 
 void Exam::saveToFile(const std::string &filename) const {
-    std::ofstream outFile(filename, std::ios::app);
+    std::ifstream inFile(filename, std::ios::app);
+    // Check if file opened successfully
+    if (!inFile.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return;
+    }
 
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(inFile, line)) {
+        for (int i = 0; i < line.length(); ++i) {
+            if (line[i] != ',' && line[i + 1] == ',') {
+                if (line.substr(i + 1) == this->examId) {
+                    line = examId + "," + "[";
+                    for (const auto &question: questions) {
+                        line += question.questionID + "-";
+                    }
+                    line += "]\n";
+                }
+            }
+        }
+        lines.push_back(line);
+    }
+    inFile.close();
+
+    std::ofstream outFile(filename, std::ios::app);
     // Check if file opened successfully
     if (!outFile.is_open()) {
         std::cerr << "Error opening file!" << std::endl;
         return;
     }
 
-    // Write exam data to csv file
-    outFile << examId << ",";                 // Exam ID
-    outFile << examName << ",";               // Exam Name
-    outFile << examDate << ",";               // Exam Date
-    outFile << examTime << ",";               // Exam Time
-    outFile << totalScore << "," << std::endl;// Total Score
+    for (const std::string &l: lines) {
+        if (!l.empty()) {
+            outFile << line << std::endl;
+        } else {
+            outFile << examId << ",";
+            outFile << "[";
+                            // Exam ID
+    		outFile << examName << ",";               // Exam Name
+    		outFile << examDate << ",";               // Exam Date
+    		outFile << examTime << ",";               // Exam Time
+      		outFile << totalScore << "," << std::endl;// Total Score
 
-    // Close the file
+            for (const auto &question: questions) {
+                outFile << question.questionID << "-";
+            }
+            outFile << "]"
+                    << "\n";
+        }
+    }
+
     outFile.close();
 }
 
-std::vector<Exam> Exam::getAll(const std::string &filename) {
-    std::ifstream inFile(filename, std::ios::app);
-    // Check if file opened successfully
-    if (!inFile.is_open()) {
-        std::cerr << "Error opening file!" << std::endl;
-    }
 
-    std::vector<Exam> exams;
-    std::string line;
-    while (std::getline(inFile, line)) {
-        std::vector<std::string> fields;
-        std::string token;
-        std::stringstream ss(line);
-        while (getline(ss, token, ',')) {
-            fields.push_back(token);
-        }
-        for (int i = 0; i < fields.size(); ++i) {
-            Exam exam(stoi(fields.at(0)), fields.at(1), fields.at(2), fields.at(3), stod(fields.at(4)));
-            exams.push_back(exam);
-        }
-    }
-    inFile.close();
-    return exams;
-}
