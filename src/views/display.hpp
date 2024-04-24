81f792479ea8452dbfee883133501eea59cc8ef3
alirhw <ali.rouhani.2005@gmail.com>
2024-04-24 22:23:35 +0430

    update menu display

--- src/views/display.hpp
+++ src/views/display.hpp
@@ -18,17 +18,30 @@ public:
     }
 
     static void menu(const std::string &username) {
-        std::cout << "Professor Dashboard! Welcome, " << username << "!" << std::endl;
-        std::cout << "1. Create New Exam" << std::endl;
-        std::cout << "2. View Exam History" << std::endl;
-        std::cout << "3. Create New Question" << std::endl;
-        std::cout << "4. Create Student Group" << std::endl;
-        std::cout << "5. View Student Groups" << std::endl;
-        std::cout << "6. Add Student To Groups" << std::endl;
-        std::cout << "7. Exam To Be Correct" << std::endl;
-        std::cout << "8. Add Student Group To Exam" << std::endl;
-        std::cout << "0. Exit" << std::endl;
-        std::cout << "Choose an option:";
+//        std::cout << "Professor Dashboard! Welcome, " << username << "!" << std::endl;
+//        std::cout << "1. Create New Exam" << std::endl;
+//        std::cout << "2. View Exam History" << std::endl;
+//        std::cout << "3. Create New Question" << std::endl;
+//        std::cout << "4. Create Student Group" << std::endl;
+//        std::cout << "5. View Student Groups" << std::endl;
+//        std::cout << "6. Add Student To Groups" << std::endl;
+//        std::cout << "7. Exam To Be Correct" << std::endl;
+//        std::cout << "8. Add Student Group To Exam" << std::endl;
+//        std::cout << "0. Exit" << std::endl;
+//        std::cout << "Choose an option:";
+		std::cout << "***************************************************"<< std::endl;
+		std::cout << "**  Professor Dashboard! Welcome, " << username << "!      **" << std::endl;
+        std::cout << "**  1. Create New Exam                           **" << std::endl;
+        std::cout << "**  2. View Exam History                         **" << std::endl;
+        std::cout << "**  3. Create New Question                       **" << std::endl;
+        std::cout << "**  4. Create Student Group                      **" << std::endl;
+        std::cout << "**  5. View Student Groups                       **" << std::endl;
+        std::cout << "**  6. Add Student To Groups                     **" << std::endl;
+        std::cout << "**  7. Exam To Be Correct                        **" << std::endl;
+        std::cout << "**  8. Add Student Group To Exam                 **" << std::endl;
+        std::cout << "**  0. Exit                                      **" << std::endl;
+        std::cout << "***************************************************"<< std::endl;
+	    std::cout << "Choose an option:";
     }
 
     static void createMultipleChoiceQuestion(std::string options[4], std::string &correctAnswer) {
